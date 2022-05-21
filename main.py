5640303263038a4fc89d8f4e2dca23c1a45af252
jasonkianhonting <70497295+jasontingkh@users.noreply.github.com>
2022-05-21 15:24:27 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from urllib import response
+import discord
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+# Defining function to extract the useful information
+
+
+def get_quote():
+    res = requests.get("https://zenquotes.io/api/random")
+    data = json.loads(res.text)
+    quote = data[0]['q'] + " -" + data[0]['a']
+    return(quote)
+
+
+def get_random_dog():
+    res = requests.get("https://random.dog/woof.json")
+    data = json.loads(res.text)
+    dog_data = data['url']
+    return dog_data
+
+
+@client.event
+async def on_ready():
+    print('Python Bot is ready: {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    if message.content.startswith('$dog'):
+        dog = get_random_dog()
+        await message.channel.send(dog)
+
+client.run(os.getenv('TOKEN'))
